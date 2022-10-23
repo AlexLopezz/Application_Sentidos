41e5aa9b760f1bc8547c37893522bf829e16f798
Daniel Alejandro Cabrera <cabrera_daniel0104@hotmail.com>
2022-10-23 16:57:57 -0300

    fix

--- Login.Designer.cs
+++ Login.Designer.cs
@@ -113,7 +113,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(383, 151);
             this.Controls.Add(this.panelLogin);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.MaximizeBox = false;
             this.Name = "Login";
             this.Text = "Inicar Sesion - Restaurante Sentidos & Casa de Te";
