5e0bceb9be49921abbac466357dbb1fb737e2887
Daniel Alejandro Cabrera <cabrera_daniel0104@hotmail.com>
2022-10-24 17:31:52 -0300

    upd logic view mozo

--- Roles/Mozo.Designer.cs
+++ Roles/Mozo.Designer.cs
@@ -37,7 +37,7 @@
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
-            this.btnConfirmar = new System.Windows.Forms.Button();
+            this.btnCargarMesa = new System.Windows.Forms.Button();
             this.btnModificarPedido = new System.Windows.Forms.Button();
             this.lblNumeroMesa = new System.Windows.Forms.Label();
             this.cboBoxMesas = new System.Windows.Forms.ComboBox();
@@ -45,10 +45,17 @@
             this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.dgvPedidoMesa = new System.Windows.Forms.DataGridView();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.dgvPedidosACerrar = new System.Windows.Forms.DataGridView();
             this.label7 = new System.Windows.Forms.Label();
             this.txtBoxCantidad = new System.Windows.Forms.TextBox();
-            this.listBoxProductos = new System.Windows.Forms.ListBox();
+            this.btnNuevoPedido = new System.Windows.Forms.Button();
+            this.btnCerrarMesa = new System.Windows.Forms.Button();
+            this.Column7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column8 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.dgvProductos)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dgvPedidoMesa)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dgvPedidosACerrar)).BeginInit();
@@ -57,7 +64,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(28, 12);
+            this.label1.Location = new System.Drawing.Point(12, 9);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(68, 15);
             this.label1.TabIndex = 0;
@@ -66,7 +73,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(28, 44);
+            this.label2.Location = new System.Drawing.Point(12, 41);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(63, 15);
             this.label2.TabIndex = 1;
@@ -84,7 +91,7 @@
             "Pescados",
             "Pizzas",
             "Snacks"});
-            this.cboBoxCategorias.Location = new System.Drawing.Point(136, 41);
+            this.cboBoxCategorias.Location = new System.Drawing.Point(120, 38);
             this.cboBoxCategorias.Name = "cboBoxCategorias";
             this.cboBoxCategorias.Size = new System.Drawing.Size(121, 23);
             this.cboBoxCategorias.TabIndex = 3;
@@ -92,7 +99,7 @@
             // 
             // btnAgregar
             // 
-            this.btnAgregar.Location = new System.Drawing.Point(263, 214);
+            this.btnAgregar.Location = new System.Drawing.Point(247, 260);
             this.btnAgregar.Name = "btnAgregar";
             this.btnAgregar.Size = new System.Drawing.Size(75, 23);
             this.btnAgregar.TabIndex = 5;
@@ -102,7 +109,7 @@
             // 
             // btnBorrar
             // 
-            this.btnBorrar.Location = new System.Drawing.Point(263, 263);
+            this.btnBorrar.Location = new System.Drawing.Point(247, 309);
             this.btnBorrar.Name = "btnBorrar";
             this.btnBorrar.Size = new System.Drawing.Size(75, 23);
             this.btnBorrar.TabIndex = 6;
@@ -131,26 +138,27 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(618, 111);
+            this.label5.Location = new System.Drawing.Point(698, 111);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(102, 15);
             this.label5.TabIndex = 13;
             this.label5.Text = "Pedidos en Espera";
             // 
-            // btnConfirmar
+            // btnCargarMesa
             // 
-            this.btnConfirmar.Location = new System.Drawing.Point(875, 111);
-            this.btnConfirmar.Name = "btnConfirmar";
-            this.btnConfirmar.Size = new System.Drawing.Size(75, 39);
-            this.btnConfirmar.TabIndex = 14;
-            this.btnConfirmar.Text = "Cerrar Mesa";
-            this.btnConfirmar.UseVisualStyleBackColor = true;
+            this.btnCargarMesa.Location = new System.Drawing.Point(537, 21);
+            this.btnCargarMesa.Name = "btnCargarMesa";
+            this.btnCargarMesa.Size = new System.Drawing.Size(103, 61);
+            this.btnCargarMesa.TabIndex = 14;
+            this.btnCargarMesa.Text = "Cargar Mesa";
+            this.btnCargarMesa.UseVisualStyleBackColor = true;
+            this.btnCargarMesa.Click += new System.EventHandler(this.btnCargarMesa_Click);
             // 
             // btnModificarPedido
             // 
-            this.btnModificarPedido.Location = new System.Drawing.Point(875, 156);
+            this.btnModificarPedido.Location = new System.Drawing.Point(698, 21);
             this.btnModificarPedido.Name = "btnModificarPedido";
-            this.btnModificarPedido.Size = new System.Drawing.Size(75, 39);
+            this.btnModificarPedido.Size = new System.Drawing.Size(103, 61);
             this.btnModificarPedido.TabIndex = 15;
             this.btnModificarPedido.Text = "Modificar Mesa";
             this.btnModificarPedido.UseVisualStyleBackColor = true;
@@ -189,7 +197,7 @@
             "18",
             "19",
             "20"});
-            this.cboBoxMesas.Location = new System.Drawing.Point(136, 9);
+            this.cboBoxMesas.Location = new System.Drawing.Point(120, 6);
             this.cboBoxMesas.Name = "cboBoxMesas";
             this.cboBoxMesas.Size = new System.Drawing.Size(121, 23);
             this.cboBoxMesas.TabIndex = 17;
@@ -197,56 +205,97 @@
             // 
             // dgvProductos
             // 
+            this.dgvProductos.AllowUserToAddRows = false;
+            this.dgvProductos.AllowUserToDeleteRows = false;
             this.dgvProductos.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dgvProductos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dgvProductos.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.Column1,
             this.Column2});
-            this.dgvProductos.Location = new System.Drawing.Point(473, 29);
+            this.dgvProductos.Location = new System.Drawing.Point(12, 141);
             this.dgvProductos.Name = "dgvProductos";
+            this.dgvProductos.ReadOnly = true;
             this.dgvProductos.RowHeadersVisible = false;
             this.dgvProductos.RowTemplate.Height = 25;
-            this.dgvProductos.Size = new System.Drawing.Size(111, 97);
+            this.dgvProductos.Size = new System.Drawing.Size(229, 304);
             this.dgvProductos.TabIndex = 18;
             // 
             // Column1
             // 
             this.Column1.HeaderText = "Producto";
             this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
             // 
             // Column2
             // 
             this.Column2.HeaderText = "Precio";
             this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
             // 
             // dgvPedidoMesa
             // 
             this.dgvPedidoMesa.AllowUserToAddRows = false;
             this.dgvPedidoMesa.AllowUserToDeleteRows = false;
+            this.dgvPedidoMesa.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dgvPedidoMesa.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvPedidoMesa.Location = new System.Drawing.Point(344, 141);
+            this.dgvPedidoMesa.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column3,
+            this.Column4,
+            this.Column5,
+            this.Column6});
+            this.dgvPedidoMesa.Location = new System.Drawing.Point(328, 141);
             this.dgvPedidoMesa.Name = "dgvPedidoMesa";
             this.dgvPedidoMesa.ReadOnly = true;
+            this.dgvPedidoMesa.RowHeadersVisible = false;
             this.dgvPedidoMesa.RowTemplate.Height = 25;
-            this.dgvPedidoMesa.Size = new System.Drawing.Size(240, 304);
+            this.dgvPedidoMesa.Size = new System.Drawing.Size(329, 304);
             this.dgvPedidoMesa.TabIndex = 19;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Producto";
+            this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Cantidad";
+            this.Column4.Name = "Column4";
+            this.Column4.ReadOnly = true;
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "Precio";
+            this.Column5.Name = "Column5";
+            this.Column5.ReadOnly = true;
+            // 
+            // Column6
+            // 
+            this.Column6.HeaderText = "Total";
+            this.Column6.Name = "Column6";
+            this.Column6.ReadOnly = true;
             // 
             // dgvPedidosACerrar
             // 
             this.dgvPedidosACerrar.AllowUserToAddRows = false;
             this.dgvPedidosACerrar.AllowUserToDeleteRows = false;
+            this.dgvPedidosACerrar.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dgvPedidosACerrar.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvPedidosACerrar.Location = new System.Drawing.Point(618, 141);
+            this.dgvPedidosACerrar.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column7,
+            this.Column8});
+            this.dgvPedidosACerrar.Location = new System.Drawing.Point(663, 141);
             this.dgvPedidosACerrar.Name = "dgvPedidosACerrar";
             this.dgvPedidosACerrar.ReadOnly = true;
+            this.dgvPedidosACerrar.RowHeadersVisible = false;
             this.dgvPedidosACerrar.RowTemplate.Height = 25;
-            this.dgvPedidosACerrar.Size = new System.Drawing.Size(240, 304);
+            this.dgvPedidosACerrar.Size = new System.Drawing.Size(287, 304);
             this.dgvPedidosACerrar.TabIndex = 20;
             // 
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(28, 78);
+            this.label7.Location = new System.Drawing.Point(12, 75);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(55, 15);
             this.label7.TabIndex = 21;
@@ -254,19 +303,42 @@
             // 
             // txtBoxCantidad
             // 
-            this.txtBoxCantidad.Location = new System.Drawing.Point(136, 75);
+            this.txtBoxCantidad.Location = new System.Drawing.Point(120, 72);
             this.txtBoxCantidad.Name = "txtBoxCantidad";
             this.txtBoxCantidad.Size = new System.Drawing.Size(121, 23);
             this.txtBoxCantidad.TabIndex = 22;
             // 
-            // listBoxProductos
+            // btnNuevoPedido
             // 
-            this.listBoxProductos.FormattingEnabled = true;
-            this.listBoxProductos.ItemHeight = 15;
-            this.listBoxProductos.Location = new System.Drawing.Point(12, 143);
-            this.listBoxProductos.Name = "listBoxProductos";
-            this.listBoxProductos.Size = new System.Drawing.Size(245, 304);
-            this.listBoxProductos.TabIndex = 23;
+            this.btnNuevoPedido.Location = new System.Drawing.Point(379, 21);
+            this.btnNuevoPedido.Name = "btnNuevoPedido";
+            this.btnNuevoPedido.Size = new System.Drawing.Size(103, 61);
+            this.btnNuevoPedido.TabIndex = 23;
+            this.btnNuevoPedido.Text = "Nuevo";
+            this.btnNuevoPedido.UseVisualStyleBackColor = true;
+            this.btnNuevoPedido.Click += new System.EventHandler(this.btnNuevoPedido_Click);
+            // 
+            // btnCerrarMesa
+            // 
+            this.btnCerrarMesa.Location = new System.Drawing.Point(847, 21);
+            this.btnCerrarMesa.Name = "btnCerrarMesa";
+            this.btnCerrarMesa.Size = new System.Drawing.Size(103, 61);
+            this.btnCerrarMesa.TabIndex = 24;
+            this.btnCerrarMesa.Text = "Cerrar Mesa";
+            this.btnCerrarMesa.UseVisualStyleBackColor = true;
+            this.btnCerrarMesa.Click += new System.EventHandler(this.btnCerrarMesa_Click_1);
+            // 
+            // Column7
+            // 
+            this.Column7.HeaderText = "N° Mesa";
+            this.Column7.Name = "Column7";
+            this.Column7.ReadOnly = true;
+            // 
+            // Column8
+            // 
+            this.Column8.HeaderText = "Total";
+            this.Column8.Name = "Column8";
+            this.Column8.ReadOnly = true;
             // 
             // Mozo
             // 
@@ -274,7 +346,8 @@
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
             this.ClientSize = new System.Drawing.Size(962, 457);
-            this.Controls.Add(this.listBoxProductos);
+            this.Controls.Add(this.btnCerrarMesa);
+            this.Controls.Add(this.btnNuevoPedido);
             this.Controls.Add(this.txtBoxCantidad);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.dgvPedidosACerrar);
@@ -283,7 +356,7 @@
             this.Controls.Add(this.cboBoxMesas);
             this.Controls.Add(this.lblNumeroMesa);
             this.Controls.Add(this.btnModificarPedido);
-            this.Controls.Add(this.btnConfirmar);
+            this.Controls.Add(this.btnCargarMesa);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
@@ -317,7 +390,7 @@
         private Label label3;
         private Label label4;
         private Label label5;
-        private Button btnConfirmar;
+        private Button btnCargarMesa;
         private Button btnModificarPedido;
         private Label lblNumeroMesa;
         private ComboBox cboBoxMesas;
@@ -328,6 +401,13 @@
         private DataGridView dgvPedidosACerrar;
         private Label label7;
         private TextBox txtBoxCantidad;
-        private ListBox listBoxProductos;
+        private DataGridViewTextBoxColumn Column3;
+        private DataGridViewTextBoxColumn Column4;
+        private DataGridViewTextBoxColumn Column5;
+        private DataGridViewTextBoxColumn Column6;
+        private Button btnNuevoPedido;
+        private Button btnCerrarMesa;
+        private DataGridViewTextBoxColumn Column7;
+        private DataGridViewTextBoxColumn Column8;
     }
 }
--- Roles/Mozo.cs
+++ Roles/Mozo.cs
@@ -5,63 +5,188 @@ namespace Application_Sentidos.Roles
 {
     public partial class Mozo : Form
     {
+        private Productos prod = new Productos();
+        List<Productos> listaProductos = new List<Productos>();
 
         HttpUser userLogged;
         public Mozo(HttpUser user)
         {
             InitializeComponent();
             userLogged = user;
+            txtBoxCantidad.Text = "1";
         }
         private void btnModificarPedido_Click(object sender, EventArgs e)
         {
             //Modifica el pedido seleccionado de la mesa x
+            if (dgvPedidosACerrar.Rows.Count == 0)
+            {
+                MessageBox.Show("Genere y cargue un pedido para moder modificar su seleccion", "Generar y cargar para Modificar");
+            }
+            else
+            {
+                cboBoxMesas.Text = dgvPedidosACerrar.CurrentRow.Cells[0].Value.ToString();
+                int nmesa = int.Parse(cboBoxMesas.Text);
+                for (int i = 0; i < listaProductos.Count; i++)
+                {
+                    if (listaProductos[i].numMesa == nmesa)
+                    {
+                        dgvPedidoMesa.Rows.Add(listaProductos[i].name, listaProductos[i].cantidad,
+                            listaProductos[i].price, listaProductos[i].totalProducto);
+                    }
+                }
+            }
         }
         private void btnAgregar_Click(object sender, EventArgs e)
         {
-            //agregar elemento seleccionado de la lista de items del menu
+            //agrega el elemento seleccionado de la lista al pedido de la mesa
+            if (dgvProductos.Rows.Count == 0)
+            {
+                MessageBox.Show("Debe agregar una categoria para poder cargar un pedido", "Falta seleccionar Categoria");
+            }
+            else if (lblNumeroMesa.Text == "-")
+            {
+                MessageBox.Show("Debe seleccionar una mesa antes de agregar un item", "Seleccion de Mesa");
+            }
+            else if (txtBoxCantidad.Text == "")
+            {
+                MessageBox.Show("Ingrese una cantidad antes de agregar el item", "Cantidad Requerida");
+            }
+            else
+            {
+                double cantidad = double.Parse(txtBoxCantidad.Text);
+                double prod = (double)dgvProductos.CurrentRow.Cells[1].Value;
+                double totalProducto = prod * cantidad;
+                dgvPedidoMesa.Rows.Add(dgvProductos.CurrentRow.Cells[0].Value, txtBoxCantidad.Text, dgvProductos.CurrentRow.Cells[1].Value, totalProducto);
+            }
         }
         private void btnBorrar_Click(object sender, EventArgs e)
         {
-            //deletear el pedido de la bd con el id o lo que sea, jeje
+            if (dgvPedidoMesa.Rows.Count == 0)
+            {
+                MessageBox.Show("Debe seleccionar un item para elimiarlo", "Seleccion");
+            }
+            else
+            {
+                dgvPedidoMesa.Rows.RemoveAt(dgvPedidoMesa.CurrentRow.Index);
+            }
         }
         public async Task cargarCategorias(string categoria)
         {
-            //var urlRequest = "https://jsonplaceholder.typicode.com/posts";
             var urlRequest = "https://binarysystem.pythonanywhere.com/api/filterCategory/?nameCategory=" + categoria;
             HttpClient client = new HttpClient();
             var httpResponse = await client.GetAsync(urlRequest);
 
             if (httpResponse.IsSuccessStatusCode)
             {
-                MessageBox.Show(urlRequest);
                 var content = await httpResponse.Content.ReadAsStringAsync();
                 List<Productos> productos = JsonSerializer.Deserialize<List<Productos>>(content);
-                MessageBox.Show("Join 2");
+
                 foreach (var item in productos)
                 {
-                    listBoxProductos.Items.Add(item.name);
+                    dgvProductos.Rows.Add(item.name, item.price);
                 }
-                MessageBox.Show("Join 3");
             }
-
         }
         private void cboBoxCategorias_SelectedIndexChanged(object sender, EventArgs e)
         {
             switch (cboBoxCategorias.Text)
             {
-                case "Bebidas sin alcohol": { cargarCategorias(cboBoxCategorias.Text); break; }
-                case "Bebidas con alcohol": { cargarCategorias(cboBoxCategorias.Text); break; }
-                case "Hamburguesas": { cargarCategorias(cboBoxCategorias.Text); break; }
-                case "Lomitos": { cargarCategorias(cboBoxCategorias.Text); break; }
-                case "Pastas": { cargarCategorias(cboBoxCategorias.Text); break; }
-                case "Pescados": { cargarCategorias(cboBoxCategorias.Text); break; }
-                case "Pizzas": { cargarCategorias(cboBoxCategorias.Text); break; }
-                case "Snacks": { cargarCategorias(cboBoxCategorias.Text); break; }
+                case "Bebidas sin alcohol":
+                    {
+                        dgvProductos.Rows.Clear();
+                        cargarCategorias(cboBoxCategorias.Text);
+                        break;
+                    }
+                case "Bebidas con alcohol":
+                    {
+                        dgvProductos.Rows.Clear();
+                        cargarCategorias(cboBoxCategorias.Text);
+                        break;
+                    }
+                case "Hamburguesas":
+                    {
+                        dgvProductos.Rows.Clear();
+                        cargarCategorias(cboBoxCategorias.Text);
+                        break;
+                    }
+                case "Lomitos":
+                    {
+                        dgvProductos.Rows.Clear();
+                        cargarCategorias(cboBoxCategorias.Text);
+                        break;
+                    }
+                case "Pastas":
+                    {
+                        dgvProductos.Rows.Clear();
+                        cargarCategorias(cboBoxCategorias.Text);
+                        break;
+                    }
+                case "Pescados":
+                    {
+                        dgvProductos.Rows.Clear();
+                        cargarCategorias(cboBoxCategorias.Text);
+                        break;
+                    }
+                case "Pizzas":
+                    {
+                        dgvProductos.Rows.Clear();
+                        cargarCategorias(cboBoxCategorias.Text);
+                        break;
+                    }
+                case "Snacks":
+                    {
+                        dgvProductos.Rows.Clear();
+                        cargarCategorias(cboBoxCategorias.Text);
+                        break;
+                    }
             }
         }
         private void cboBoxMesas_SelectedIndexChanged(object sender, EventArgs e)
         {
             lblNumeroMesa.Text = cboBoxMesas.Text;
+        }
+        private void btnCargarMesa_Click(object sender, EventArgs e)
+        {
+            double total = 0;
+            if (dgvPedidoMesa.Rows.Count == 0)
+            {
+                MessageBox.Show("Debe generar un pedido antes de cargarlo", "Carga de Pedido Requerida");
+            }
+            else
+            {
+                foreach (DataGridViewRow row in dgvPedidoMesa.Rows)
+                {
+                    total += (double)row.Cells[3].Value;
+                }
+                dgvPedidosACerrar.Rows.Add(lblNumeroMesa.Text, total);
+
+
+                foreach (DataGridViewRow row in dgvPedidoMesa.Rows)
+                {
+                    prod.numMesa = int.Parse(cboBoxMesas.Text);
+                    prod.name = row.Cells[0].Value.ToString();
+                    prod.cantidad = row.Cells[1].Value.ToString();
+                    prod.price = (double)row.Cells[2].Value;
+                    prod.totalProducto = (double)row.Cells[3].Value;
+                    listaProductos.Add(prod);
+                }
+            }
+
+        }
+
+        private void btnNuevoPedido_Click(object sender, EventArgs e)
+        {
+            dgvProductos.Rows.Clear();
+            dgvPedidoMesa.Rows.Clear();
+            cboBoxCategorias.Text = "";
+            cboBoxMesas.Text = "";
+            txtBoxCantidad.Clear();
+            lblNumeroMesa.Text = "";
+        }
+
+        private void btnCerrarMesa_Click_1(object sender, EventArgs e)
+        {
+
         }
         private void Mozo_Load(object sender, EventArgs e)
         {
@@ -74,16 +199,13 @@ namespace Application_Sentidos.Roles
     }
     public class Productos
     {
+        public int id { get; set; }
         public string name { get; set; }
         public string description { get; set; }
-        public int price { get; set; }
+        public double price { get; set; }
         public string img { get; set; }
-    }
-    public class Post
-    {
-        public int userId { get; set; }
-        public int id { get; set; }
-        public string title { get; set; }
-        public string body { get; set; }
+        public string cantidad { get; set; }
+        public double totalProducto { get; set; }
+        public int numMesa { get; set; }
     }
 }
